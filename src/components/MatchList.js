e455b8eb9538f5b62dabcab83600184943822cbb
open-meadow <rohan.varughese@protonmail.com>
2023-01-08 14:42:06 -0500

    create components

---
+++ src/components/MatchList.js
@@ -0,0 +1,6 @@
+const MatchList = () => {
+  <section className="PlayerList MatchList">
+    <h1>Match list</h1>
+    {/* Matches will be shown here */}
+  </section>;
+};
---
+++ src/components/PlayerList.js
@@ -0,0 +1,8 @@
+const PlayerList = () => {
+  return (
+    <section className="PlayerList">
+      <h1>Current Participating Players</h1>
+      {/* Players will be shown here */}
+    </section>
+  )
+}
